8bc1b743515959c4e8a7c23c87a1f713d27b9f77
IshaanShaffir <75286144+IshaanShaffir@users.noreply.github.com>
2025-01-28 22:12:01 -0800

    Added WASD console functionality to BallController

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,12 +5,26 @@ public class BallController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Calling the Start method");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Calling the Update method");
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log(Vector2.up);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log(Vector2.down);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log(Vector2.right);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log(Vector2.left);
+        }
     }
 }
